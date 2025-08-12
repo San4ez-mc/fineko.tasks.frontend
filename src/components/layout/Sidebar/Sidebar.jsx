dae54025629532ad5e659ce654d1c5ce3ceb8480
San4ez-mc <olexandrmatsuk@gmail.com>
2025-08-12 13:54:34 +0300

    refactor: remove duplicate nav click handler

--- src/components/layout/Sidebar/Sidebar.jsx
+++ src/components/layout/Sidebar/Sidebar.jsx
@@ -38,12 +38,6 @@ export default function Sidebar({
         }
     };
 
-    const handleNavClick = () => {
-        if (isOpen) {
-            onToggle();
-        }
-    };
-
     return (
         <aside className={`sidebar ${isOpen ? "expanded" : "collapsed"}`}>
             <div className="sidebar-content">
