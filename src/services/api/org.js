9229ea960ef02dc53a58843376a2ff31ea086a3c
San4ez-mc <olexandrmatsuk@gmail.com>
2025-09-03 21:18:11 +0300

    feat(org): додати компонент OrgTable

--- src/services/api/org.js
+++ src/services/api/org.js
@@ -3,5 +3,6 @@ import api from "../api";
 export const createPosition = (data) => api.post("/org/position", data).then(r => r.data);
 export const createDepartment = (data) => api.post("/org/department", data).then(r => r.data);
 export const updatePosition = (id, patch) => api.patch(`/org/position/${id}`, patch).then(r => r.data);
+export const getFlat = (params = {}) => api.get("/org/flat", { params }).then(r => r.data);
 
-export default { createPosition, createDepartment, updatePosition };
+export default { createPosition, createDepartment, updatePosition, getFlat };
